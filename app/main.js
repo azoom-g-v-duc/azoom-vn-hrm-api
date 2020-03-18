c0bf4c6ef948ba3e37715939db3ef107f6bab46b
azoom-v-t-dung <v.t.dung@azoom.jp>
2020-03-18 09:41:31 +0700

    update structure

--- app/main.js
+++ app/main.js
@@ -29,7 +29,6 @@ express.response.sendStatus = function (statusCode) {
 }
 
 const app = express()
-
 app.use(bodyParser.json({ limit: '50mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 app.use(
