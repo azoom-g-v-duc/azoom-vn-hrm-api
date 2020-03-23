9ed079ff807cebba5034250229e82ffdc5188f8f
azoom-v-t-dung <v.t.dung@azoom.jp>
2020-03-23 08:33:17 +0700

    create login api

--- app/routes/authentications/logout/post.js
+++ app/routes/authentications/logout/post.js
@@ -3,12 +3,9 @@ import {
   destroyALLTokenOfUser
 } from '@cloudStoreDatabase/token-user'
 
-export default async function (req, res) {
   const { isAll = false } = req.query
   if (isAll) {
     await destroyALLTokenOfUser(req.user.id)
   } else {
     await destroyToken(req.token.tokenCode)
   }
-  return res.send({ message: 'Logout successfully' })
-}
