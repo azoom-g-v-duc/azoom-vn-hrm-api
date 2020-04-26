58b38f8f17b20c9cc894de93862b3d9269d1379e
azoom-g-v-duc <61811114+azoom-g-v-duc@users.noreply.github.com>
2020-04-26 17:24:32 +0700

    :hammer: Refactor applications/leaves POST api (#33)
    :hammer: migrate firestore-database into apis

--- app/fb-cloud-store-database/timesheet-application.js
+++ app/fb-cloud-store-database/timesheet-application.js
@@ -121,34 +121,6 @@ export const getAllTsAppUser = async (page, number, userId) => {
   return result
 }
 
-export const getAllTsAppUser = async (page, number, userId) => {
-  const result = { count: 0, data: [] }
-  const query = await getTable(process.env.DB_TABLE_TIME_SHEET_APPLICATION)
-    .where('userId', '==', userId)
-    .orderBy('created', 'desc')
-  const datall = await query.get()
-  result.count = datall.empty ? 0 : await datall.docs.length
-  if (!page) {
-    result.data = datall.empty ? '' : await datall.docs.map(doc => doc.data())
-    return result
-  }
-  if (page && number && page * number - 1 <= result.count) {
-    const queryData = await query
-      .startAt(
-        await datall.docs[page - 1 ? (page - 1) * number : page - 1].data()
-          .created
-      )
-      .limit(number)
-      .get()
-    result.data = queryData.empty
-      ? []
-      : await queryData.docs.map(doc => doc.data())
-    return result
-  }
-
-  return result
-}
-
 export const updateTsApp = async dataReq => {
   const queryData = await timesheetAppCollection()
     .where('id', '==', dataReq.id)
--- app/routes/authentications/logout/post.js
+++ app/routes/authentications/logout/post.js
@@ -1,11 +1,11 @@
-import {
-  destroyToken,
-  destroyALLTokenOfUser
-} from '@cloudStoreDatabase/token-user'
+import { destroyToken, destroyALLTokenOfUser } from '@helpers/token-user'
 
+export default async (req, res) => {
   const { isAll = false } = req.query
   if (isAll) {
     await destroyALLTokenOfUser(req.user.id)
   } else {
     await destroyToken(req.token.tokenCode)
   }
+  return res.send({ message: 'Logout successfully' })
+}
